d8e9e2a08d9f018df136b3fc418863ac8b2b17ee
Ruiyu <ruiyuwu1998@gmail.com>
2019-06-17 14:13:29 -0400

    Calculate bonuses

---
+++ Inheritance/employee.rb
@@ -0,0 +1,14 @@
+class Employee
+  attr_reader :salary
+
+  def initialize(name, title, salary, boss) 
+    @name = name 
+    @title = title 
+    @salary = salary
+    @boss = boss
+  end
+
+  def bonus(multiplier)
+    self.salary * multiplier
+  end
+end
